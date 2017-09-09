a404c32864f378d238314308685017c92400a6eb
Scott Mikula <smikula@microsoft.com>
2017-09-09 15:23:04 -0700

    Resolve each import in each file

--- src/validate.ts
+++ src/validate.ts
@@ -1,8 +1,33 @@
+import * as fs from 'fs';
 import * as glob from 'glob';
+import * as ts from 'typescript';
+import getCompilerOptions from './getCompilerOptions';
 
 export default function validate() {
     let files = glob.sync('src/**/*.ts');
+
+    const compilerOptions = getCompilerOptions();
+    let compilerHost = ts.createCompilerHost(compilerOptions);
+
     files.forEach(file => {
-        console.log(file);
+        let fileInfo = ts.preProcessFile(fs.readFileSync(file).toString(), true, true);
+        console.log('Validating file:', file);
+
+        fileInfo.importedFiles.forEach(importInfo => {
+            let resolvedFile = ts.resolveModuleName(
+                importInfo.fileName,
+                file,
+                compilerOptions,
+                compilerHost,
+                null // TODO
+            );
+
+            console.log(
+                'resolvedModule:',
+                resolvedFile.resolvedModule && resolvedFile.resolvedModule.resolvedFileName
+            );
+        });
+
+        console.log();
     });
 }
