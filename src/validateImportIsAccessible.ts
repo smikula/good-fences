dc3b5ec90e21569e960fb0039f30d68d29005e57
Scott Mikula <smikula@microsoft.com>
2017-10-03 16:56:34 -0700

    Update export rule validation

--- src/validateImportIsAccessible.ts
+++ src/validateImportIsAccessible.ts
@@ -1,44 +1,53 @@
 import * as path from 'path';
+import Config from './types/Config';
 import getConfigsForFile from './getConfigsForFile';
 import reportError from './reportError';
 
 export default function validateImportIsAccessible(sourceFile: string, importFile: string) {
-    let configsForImport = getConfigsForFile(importFile);
-    let isExported = false;
-    let foundUnsharedConfig = false;
-
     // Make sure we're using absolute paths
     sourceFile = path.resolve(sourceFile);
     importFile = path.resolve(importFile);
 
-    // Process each config that applies to the imported file
-    configsForImport.forEach(config => {
-        // If the config is shared between source file and import then the export rules don't apply
-        if (path.relative(config.path, sourceFile).startsWith('..')) {
-            foundUnsharedConfig = true;
+    // Validate against each config that applies to the imported file
+    let configsForImport = getConfigsForFile(importFile);
+    configsForImport.forEach(config => validateConfig(config, sourceFile, importFile));
+}
 
-            // Examine each export specified in the config
-            if (config.exports) {
-                Object.keys(config.exports).forEach(key => {
-                    let value = config.exports[key];
+function validateConfig(config: Config, sourceFile: string, importFile: string) {
+    // If the source file is under the config (i.e. the source and import files share the
+    // config) then we don't apply the export rules
+    if (!path.relative(config.path, sourceFile).startsWith('..')) {
+        return;
+    }
 
-                    if (
-                        keyMatchesImportFile(config.path, key, importFile) &&
-                        valueMatchesSourceFile(value, sourceFile)
-                    ) {
-                        isExported = true;
-                    }
-                });
-            }
+    // If the config doesn't specify exports then everything is considered public
+    if (!config.exports) {
+        return;
+    }
+
+    // See if the config has an export rule that matches
+    if (hasMatchingExport(config, sourceFile, importFile)) {
+        return;
+    }
+
+    // If we made it here, the import is invalid
+    reportError(`${sourceFile} is importing inaccessible module ${importFile}`);
+}
+
+function hasMatchingExport(config: Config, sourceFile: string, importFile: string) {
+    let isExported = false;
+    Object.keys(config.exports).forEach(key => {
+        let value = config.exports[key];
+
+        if (
+            keyMatchesImportFile(config.path, key, importFile) &&
+            valueMatchesSourceFile(value, sourceFile)
+        ) {
+            isExported = true;
         }
     });
 
-    // If there is no unshared config (i.e. both files are under the same subtree of every config
-    // above them) then the import is valid by default.  Otherwise we need to make sure that it
-    // was exported.
-    if (foundUnsharedConfig && !isExported) {
-        reportError(`${sourceFile} is not allowed to import ${importFile}`);
-    }
+    return isExported;
 }
 
 function keyMatchesImportFile(configPath: string, key: string, importFile: string) {
