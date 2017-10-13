77cb4c5b56e26015643286098017a88a01f240f5
Scott Mikula <smikula@microsoft.com>
2017-10-12 21:21:47 -0700

    Add tests for fileMatchesTag

--- test/fileMatchesTagTests.ts
+++ test/fileMatchesTagTests.ts
@@ -1,7 +1,31 @@
 import fileMatchesTag from '../src/fileMatchesTag';
+import * as getTagsForFile from '../src/getTagsForFile';
+
+const MATCHING_TAG = 'matchingTag';
+const NONMATCHING_TAG = 'nonmatchingTag';
 
 describe('fileMatchesTag', () => {
+    beforeEach(() => {
+        spyOn(getTagsForFile, 'default').and.returnValue([MATCHING_TAG]);
+    });
+
+    it('returns false if no tag matches', () => {
+        let match = fileMatchesTag(null, NONMATCHING_TAG);
+        expect(match).toBe(false);
+    });
+
     it('matches anything to *', () => {
-        expect(fileMatchesTag(null, '*')).toBe(true);
+        let match = fileMatchesTag(null, '*');
+        expect(match).toBe(true);
+    });
+
+    it('matches a single tag', () => {
+        let match = fileMatchesTag(null, MATCHING_TAG);
+        expect(match).toBe(true);
+    });
+
+    it('matches an array of tags', () => {
+        let match = fileMatchesTag(null, [NONMATCHING_TAG, MATCHING_TAG]);
+        expect(match).toBe(true);
     });
 });
