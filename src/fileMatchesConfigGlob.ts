f31326396acc698231f70b03abf60fbd8ff80dd6
Scott Mikula <smikula@microsoft.com>
2017-10-12 22:38:39 -0700

    Finish implementing fileMatchesConfigGlob

--- src/fileMatchesConfigGlob.ts
+++ src/fileMatchesConfigGlob.ts
@@ -1,7 +1,13 @@
 import * as path from 'path';
+const minimatch = require('minimatch');
 
 export default function fileMatchesConfigGlob(importFile: string, configPath: string, key: string) {
+    // '*' matches all files under the config
+    if (key == '*') {
+        return true;
+    }
+
     // Remove the file extension before matching
     importFile = importFile.substr(0, importFile.length - path.extname(importFile).length);
-    return path.resolve(configPath, key) == importFile;
+    return minimatch(importFile, path.resolve(configPath, key));
 }
