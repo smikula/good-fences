0ec26c5b053d2459352275e02d59e57cd1b09d8a
Scott Mikula <mikula@gmail.com>
2017-10-23 11:19:35 -0700

    Remove excess logging

--- src/validateFile.ts
+++ src/validateFile.ts
@@ -2,15 +2,9 @@ import * as fs from 'fs';
 import * as ts from 'typescript';
 import getCompilerHost from './getCompilerHost';
 import getCompilerOptions from './getCompilerOptions';
-import getTagsForFile from './getTagsForFile';
 import validateImportIsAccessible from './validateImportIsAccessible';
 
 export default function validateFile(filePath: string) {
-    console.log('Validating file:', filePath);
-
-    let tags = getTagsForFile(filePath);
-    console.log('  Tags: ', tags);
-
     let fileInfo = ts.preProcessFile(fs.readFileSync(filePath).toString(), true, true);
     fileInfo.importedFiles.forEach(importInfo => {
         let resolvedFile = ts.resolveModuleName(
@@ -21,15 +15,8 @@ export default function validateFile(filePath: string) {
             null // TODO
         );
 
-        console.log(
-            '  resolvedModule: ',
-            resolvedFile.resolvedModule && resolvedFile.resolvedModule.resolvedFileName
-        );
-
         if (resolvedFile.resolvedModule) {
             validateImportIsAccessible(filePath, resolvedFile.resolvedModule.resolvedFileName);
         }
     });
-
-    console.log();
 }
