3271d1e2c5dafc4b111a10ef62a7aa6cbdd8b0c8
Scott Mikula <mikula@gmail.com>
2017-10-25 16:37:32 -0700

    Fix tsconfig filename

--- src/runner.ts
+++ src/runner.ts
@@ -2,7 +2,7 @@ import validateFile from './validateFile';
 import TypeScriptProgram from './TypeScriptProgram';
 
 export function run() {
-    let tsProgram = new TypeScriptProgram('tsconfig.build.json');
+    let tsProgram = new TypeScriptProgram('tsconfig.json');
     let files = tsProgram.getSourceFiles();
     files.forEach(file => {
         validateFile(file, tsProgram);
