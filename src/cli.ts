39fcc8118f478cdd8714a226cc5368e1d690a2c1
Scott Mikula <mikula@gmail.com>
2017-10-26 11:00:01 -0700

    Use commander for CLI parsing

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,12 @@
+import * as commander from 'commander';
 import { run } from './runner';
+
+// Read the package version from package.json
+let packageVersion = require('../package').version;
+
+commander
+    .version(packageVersion)
+    .option('-p, --project <string>', 'tsconfig.json file')
+    .parse(process.argv);
 
 run();
