3560717ad26a608131d101f8848247e445ca845e
Scott Mikula <mikula@gmail.com>
2017-11-04 19:31:08 -0700

    Normalize and apply default options

--- src/getOptions.ts
+++ src/getOptions.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import Options from './types/Options';
 
 let options: Options;
@@ -6,6 +7,12 @@ export default function getOptions() {
     return options;
 }
 
-export function setOptions(value: Options) {
-    options = value;
+export function setOptions(providedOptions: Options) {
+    options = providedOptions;
+
+    // Normalize and apply defaults
+    options.rootDir = options.rootDir ? path.resolve(options.rootDir) : path.resolve();
+    options.project = options.project
+        ? path.resolve(options.project)
+        : path.resolve(options.rootDir, 'tsconfig.json');
 }
