ef24da157ea7e38939d55208fe971748693464aa
Scott Mikula <mikula@gmail.com>
2017-11-04 18:22:24 -0700

    Glob for fences from the project root directory

--- src/getAllConfigs.ts
+++ src/getAllConfigs.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as glob from 'glob';
 import * as path from 'path';
 import ConfigSet from './types/ConfigSet';
+import getOptions from './getOptions';
 
 let configSet: ConfigSet = null;
 
@@ -9,7 +10,13 @@ export default function getAllConfigs(): ConfigSet {
     if (!configSet) {
         configSet = {};
 
-        let files = glob.sync('**/fence.json');
+        // Glob for configs under the project root directory
+        let globString = '**/fence.json';
+        if (getOptions().rootDir) {
+            globString = path.resolve(getOptions().rootDir, globString);
+        }
+
+        let files = glob.sync(globString);
         files.forEach(file => {
             let absolutePath = path.resolve(path.dirname(file));
             configSet[absolutePath] = JSON.parse(fs.readFileSync(file).toString());
