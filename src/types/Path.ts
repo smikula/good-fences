ba47460eb2c23f07ffd620eb068ae15d648daadc
Scott Mikula <mikula@gmail.com>
2017-11-08 12:49:47 -0800

    Minor name change

--- src/types/Path.ts
+++ src/types/Path.ts
@@ -1,7 +1,7 @@
 // Simulate nominal typing
 // https://github.com/Microsoft/TypeScript/issues/202#issuecomment-302402671
 export declare class Nominal<T extends string> {
-    private as: T;
+    private nominalType: T;
 }
 
 type Path = string & Nominal<'Path'>;
