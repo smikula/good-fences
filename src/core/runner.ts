9d776533f8f3904dd499ea3c4169c806929414c5
Scott Mikula <mikula@gmail.com>
2019-07-18 15:00:15 -0700

    Warn that onError option is deprecated

--- src/core/runner.ts
+++ src/core/runner.ts
@@ -8,6 +8,11 @@ export function run(rawOptions: RawOptions) {
     // Store options so they can be globally available
     setOptions(rawOptions);
 
+    // Warn when using a deprecated option
+    if (getOptions().onError) {
+        console.warn('The onError option is deprecated.  Use the return value from run() instead.');
+    }
+
     // Run validation
     let tsProgram = new TypeScriptProgram(getOptions().project);
     let files = tsProgram.getSourceFiles();
