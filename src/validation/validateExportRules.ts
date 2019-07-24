04f45baa48c10bf6a193ebcaf9b1821cd1ada71d
Scott Mikula <mikula@gmail.com>
2019-07-24 11:25:06 -0500

    Report raw import in a consistent way

--- src/validation/validateExportRules.ts
+++ src/validation/validateExportRules.ts
@@ -5,17 +5,18 @@ import getConfigsForFile from '../utils/getConfigsForFile';
 import fileMatchesConfigGlob from '../utils/fileMatchesConfigGlob';
 import fileMatchesTag from '../utils/fileMatchesTag';
 import { reportError } from '../core/result';
+import ImportRecord from '../core/ImportRecord';
 
 export default function validateExportRules(
     sourceFile: NormalizedPath,
-    importFile: NormalizedPath
+    importRecord: ImportRecord
 ) {
     // Validate against each config that applies to the imported file
-    let configsForImport = getConfigsForFile(importFile);
-    configsForImport.forEach(config => validateConfig(config, sourceFile, importFile));
+    let configsForImport = getConfigsForFile(importRecord.filePath);
+    configsForImport.forEach(config => validateConfig(config, sourceFile, importRecord));
 }
 
-function validateConfig(config: Config, sourceFile: NormalizedPath, importFile: NormalizedPath) {
+function validateConfig(config: Config, sourceFile: NormalizedPath, importRecord: ImportRecord) {
     // If the source file is under the config (i.e. the source and import files share the
     // config) then we don't apply the export rules
     if (!path.relative(config.path, sourceFile).startsWith('..')) {
@@ -28,12 +29,12 @@ function validateConfig(config: Config, sourceFile: NormalizedPath, importFile: 
     }
 
     // See if the config has an export rule that matches
-    if (hasMatchingExport(config, sourceFile, importFile)) {
+    if (hasMatchingExport(config, sourceFile, importRecord.filePath)) {
         return;
     }
 
     // If we made it here, the import is invalid
-    reportError('Module is not exported', sourceFile, importFile, config);
+    reportError('Module is not exported', sourceFile, importRecord, config);
 }
 
 function hasMatchingExport(config: Config, sourceFile: NormalizedPath, importFile: NormalizedPath) {
