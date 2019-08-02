1e24df5cd2f35b80ae8a43f92b4aec2d79786719
Scott Mikula <mikula@gmail.com>
2019-08-02 15:37:42 -0700

    Export result types from the index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { default as GoodFencesError } from './types/GoodFencesError';
+export { default as GoodFencesResult } from './types/GoodFencesResult';
+export { default as GoodFencesWarning } from './types/GoodFencesWarning';
 export { default as Options } from './types/RawOptions';
 export { run } from './core/runner';
