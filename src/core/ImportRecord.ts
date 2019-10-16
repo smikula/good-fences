a419c7d3c6ee86379d0ee8ec5e60a67006f35638
brianmar <brianmar@microsoft.com>
2019-10-15 17:48:34 -0700

    Consider any import outside rootDir external

--- src/core/ImportRecord.ts
+++ src/core/ImportRecord.ts
@@ -2,6 +2,7 @@ import NormalizedPath from '../types/NormalizedPath';
 import TypeScriptProgram from './TypeScriptProgram';
 import normalizePath from '../utils/normalizePath';
 import * as path from 'path';
+import getOptions from '../utils/getOptions';
 
 export default class ImportRecord {
     public filePath: NormalizedPath;
@@ -17,8 +18,8 @@ export default class ImportRecord {
         }
     }
 
-    // Is this import an external dependency (i.e. is it under node_modules)?
+    // Is this import an external dependency (i.e. is it under node_modules or outside the rootPath)?
     get isExternal() {
-        return this.filePath.split(path.sep).indexOf('node_modules') != -1;
+        return this.filePath.split(path.sep).indexOf('node_modules') != -1 || !this.filePath.startsWith(getOptions().rootDir);
     }
 }
