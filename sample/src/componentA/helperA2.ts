7a45f5722f424e1be44fb8342db4cdda34d10abe
Alec Jacob Deitloff <alec.deitloff@gmail.com>
2021-02-10 13:35:00 -0800

    Add examples of making specifically-applicable tabs to sample project

--- sample/src/componentA/helperA2.ts
+++ sample/src/componentA/helperA2.ts
@@ -1,1 +1,7 @@
-export default function helperA2() {}
+import { util1 } from "../utils/util1"; // INVALID IMPORT
+import { util2ForA } from "../utils/util2.forA";
+
+export default function helperA2() {
+  util1();
+  util2ForA();
+}
