7527fd650ec3c489e95855454eb3a19f4409716b
Maxwell Huang-Hobbs <mahuangh@microsoft.com>
2021-08-06 12:46:42 -0400

    lazy load configs

--- src/utils/getConfigsForFile.ts
+++ src/utils/getConfigsForFile.ts
@@ -2,22 +2,24 @@ import * as path from 'path';
 import Config from '../types/config/Config';
 import NormalizedPath from '../types/NormalizedPath';
 import normalizePath from './normalizePath';
-import getAllConfigs from './getAllConfigs';
+import getConfigManager from './getConfigManager';
 
-// Returns an array of all the configs that apply to a given file
-export default function getConfigsForFile(filePath: NormalizedPath): Config[] {
-    let allConfigs = getAllConfigs();
-    let configsForFile: Config[] = [];
+export function getConfigPathCandidatesForFile(filePath: NormalizedPath): string[] {
+    const candidates: string[] = [];
 
     let pathSegments = normalizePath(path.dirname(filePath)).split(path.sep);
     while (pathSegments.length) {
         let dirPath = pathSegments.join(path.sep);
-        if (allConfigs[dirPath]) {
-            configsForFile.push(allConfigs[dirPath]);
-        }
-
+        candidates.push(dirPath);
         pathSegments.pop();
     }
 
-    return configsForFile;
+    return candidates;
+}
+
+// Returns an array of all the configs that apply to a given file
+export default function getConfigsForFile(filePath: NormalizedPath): Config[] {
+    const partialFenceSet = getConfigManager().partialConfigSetForPath(filePath);
+
+    return Object.entries(partialFenceSet).map(([_configPath, config]) => config);
 }
