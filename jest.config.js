155cf8f8b54e4e3ec4a2f9d8267250f770deead7
Maxwell Huang-Hobbs <mahuangh@microsoft.com>
2021-08-11 18:35:59 -0400

    add json to the moduleFileExtensions in jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     testEnvironment: 'node',
     moduleDirectories: ['node_modules'],
-    moduleFileExtensions: ['ts', 'js'],
+    moduleFileExtensions: [
+        'ts',
+        'js',
+        // nodegit parses its own package.json file,
+        // so we need this moduleFileExtension to be
+        // specified for tests.
+        'json',
+    ],
     transform: {
         '.ts': '<rootDir>/jest.preprocessor.js',
     },
