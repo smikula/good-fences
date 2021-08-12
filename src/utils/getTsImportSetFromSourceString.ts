bae49ea16013df21ec42f0e7c44729387ab54a8e
Maxwell Huang-Hobbs <mahuangh@microsoft.com>
2021-08-12 16:48:24 -0400

    remove commented code from getTsImportSetFromSourceString

--- src/utils/getTsImportSetFromSourceString.ts
+++ src/utils/getTsImportSetFromSourceString.ts
@@ -1,32 +1,6 @@
-import {
-    createSourceFile,
-    isImportDeclaration,
-    isStringLiteral,
-    preProcessFile,
-    ScriptTarget,
-    SourceFile,
-} from 'typescript';
+import { preProcessFile } from 'typescript';
 
 export function getTsImportSetFromSourceString(tsSource: string): Set<string> {
     let fileInfo = preProcessFile(tsSource, true, true);
     return new Set(fileInfo.importedFiles.map(importedFile => importedFile.fileName));
-
-    // const sourceFile: SourceFile = createSourceFile(
-    //     'mock-file',
-    //     tsSource,
-    //     ScriptTarget.Latest // langugeVersion
-    // );
-
-    // const importSet = new Set<string>();
-
-    // sourceFile.forEachChild(c => {
-    //     if (isImportDeclaration(c)) {
-    //         if (!isStringLiteral(c.moduleSpecifier)) {
-    //             throw new Error('encountered dynamic import? ' + c.moduleSpecifier.getFullText());
-    //         }
-    //         importSet.add(c.moduleSpecifier.text);
-    //     }
-    // });
-
-    // return importSet;
 }
