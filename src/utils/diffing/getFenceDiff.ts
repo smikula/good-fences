dbe6a8f39b4f3b66504a8fe60acc0931e535f415
Maxwell Huang-Hobbs <mahuangh@microsoft.com>
2021-09-01 15:58:44 -0400

    clarifying comments around fenceDiff

--- src/utils/diffing/getFenceDiff.ts
+++ src/utils/diffing/getFenceDiff.ts
@@ -21,10 +21,31 @@ export type FenceDiff = {
 };
 
 type DiffList<T> = {
+    /**
+     * The list of new entries in the new version of the list.
+     *
+     * If there is no new version of the list (e.g. the list was
+     * deleted in this change), this will be null.
+     */
     added: T[] | null;
+    /**
+     * The list of old entries in the old version of the list.
+     *
+     * If there was no old version of the list (e.g. the list was
+     * introduced in this change), this will be null.
+     */
     removed: T[] | null;
 };
 
+/**
+ * Gets a difference between the entries of the previous and new list.
+ *
+ * @param oldList - the old list, or null if the old list did not exist
+ * @param newList - the new list, or null if the old list did not exist
+ * @param checkIsSame - a method for comparing objects (e.g. for equivalent
+ *   objects that are not referentially equal)
+ * @returns a DiffList
+ */
 export function diffList<T>(
     oldList: T[] | null,
     newList: T[] | null,
@@ -66,6 +87,17 @@ export function diffList<T>(
     }
 }
 
+/**
+ * Gets the difference between two fences, which can be used to calculate a partial
+ * set of fences and source files that need to be re-checked.
+ *
+ * Each entry of the resuting FenceDiff object will be a DiffList with an added: and removed:
+ * section that hold the added and removed list entries. If the list itself was added or
+ * removed the new list will be in the corresponding added: or removed: section, and the
+ * other entry will be null.
+ *
+ * @see DiffList
+ */
 export function getFenceDiff(oldFence: Config, newFence: Config): FenceDiff | null {
     let diff: FenceDiff = {
         tags: diffList(oldFence.tags, newFence.tags, (a: string, b: string) => a === b),
